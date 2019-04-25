e551ee59b6eb5352772bbf1da2b9bf4e7b2595b1
Spencer Peters <36678862+SpencerTP31@users.noreply.github.com>
2019-04-25 01:33:10 -0400

    Data for Division Tables
    Updated cause my dumbass didn't realize division code was an int

--- divisions.sql
+++ divisions.sql
@@ -1,8 +1,8 @@
-insert into divisions (DIV_CODE, DIV_NAME) values ('AFC', 'East');
-insert into divisions (DIV_CODE, DIV_NAME) values ('NFC', 'North');
-insert into divisions (DIV_CODE, DIV_NAME) values ('NFC', 'West');
-insert into divisions (DIV_CODE, DIV_NAME) values ('AFC', 'East');
-insert into divisions (DIV_CODE, DIV_NAME) values ('AFC', 'South');
-insert into divisions (DIV_CODE, DIV_NAME) values ('AFC', 'North');
-insert into divisions (DIV_CODE, DIV_NAME) values ('NFC', 'South');
-insert into divisions (DIV_CODE, DIV_NAME) values ('AFC', 'West');
+insert into divisions (DIV_CODE, DIV_NAME) values (1, 'NFC East');
+insert into divisions (DIV_CODE, DIV_NAME) values (2, 'NFC North');
+insert into divisions (DIV_CODE, DIV_NAME) values (3, 'NFC West');
+insert into divisions (DIV_CODE, DIV_NAME) values (4, 'NFC South');
+insert into divisions (DIV_CODE, DIV_NAME) values (5, 'AFC South');
+insert into divisions (DIV_CODE, DIV_NAME) values (6, 'AFC North');
+insert into divisions (DIV_CODE, DIV_NAME) values (7, 'AFC East');
+insert into divisions (DIV_CODE, DIV_NAME) values (8, 'AFC West');
