e59c6380caec9cb069425ce817c9821e1c7f2b38
Spencer Peters <36678862+SpencerTP31@users.noreply.github.com>
2019-04-25 01:15:52 -0400

    divisions_table_data
    8 Rows of Data

---
+++ divisions.sql
@@ -0,0 +1,8 @@
+insert into divisions (DIV_CODE, DIV_NAME) values ('AFC', 'East');
+insert into divisions (DIV_CODE, DIV_NAME) values ('NFC', 'North');
+insert into divisions (DIV_CODE, DIV_NAME) values ('NFC', 'West');
+insert into divisions (DIV_CODE, DIV_NAME) values ('AFC', 'East');
+insert into divisions (DIV_CODE, DIV_NAME) values ('AFC', 'South');
+insert into divisions (DIV_CODE, DIV_NAME) values ('AFC', 'North');
+insert into divisions (DIV_CODE, DIV_NAME) values ('NFC', 'South');
+insert into divisions (DIV_CODE, DIV_NAME) values ('AFC', 'West');
