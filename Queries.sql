2d6abf9f3b8a4ac9b4419da54a81b27e105dfb76
sktt1Bang <mathewdrago@gmail.com>
2019-04-28 21:52:06 -0400

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -14,5 +14,22 @@ JOIN members AS M ON P.MEMBER_NUM = M.MEMBER_NUM
 JOIN teams AS T ON T.TEAM_CODE = M.TEAM_CODE
 WHERE TEAM_NAME = "Cowboys";
 
-
+/* Displaying the first and last name of players of the team that are in California*/
+# 3. 
+SELECT M.MEMBER_FNAME AS "FirstName", M.MEMBER_LNAME AS "LastName"  
+FROM members AS M
+JOIN teams AS T ON M.TEAM_CODE= T.TEAM_CODE
+WHERE TEAM_STATE='CA';
+/* Counting the number of Sponsors that Patriots have, I could have done it without a join but figured using join would show that we have a better knowledge of using join*/
+ # 4.
+SELECT COUNT(S.SPON_NUMBER) AS "Number of Sponsors for Patriots"
+FROM sponsors AS S 
+JOIN teams AS T ON S.TEAM_CODE = T.TEAM_CODE 
+WHERE T.TEAM_CODE = "10";                          
+/* Displaying all the teams that are in the NFC East   */                          
+#5 . 
+SELECT T.T_NAME AS "Teams from the NFC East"
+FROM teams AS T                          
+JOIN divisions AS D ON T.DIV_CODE = D.DIV_CODE
+WHERE D.DIV_CODE = "1";                           
  
