e1ad1d8e46a9d50b0a134b4cfcd1ed8b876f21e0
sktt1Bang <mathewdrago@gmail.com>
2019-04-28 21:01:32 -0400

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,16 +1,16 @@
 /* Finding the cost of the Ravens Team*/
-1. SELECT OWNER_BUYPRICE
+ SELECT OWNER_BUYPRICE
        FROM Owners AS O JOIN Members AS M ON O.MEMBER_NUM = M.MEMBER_NUM 
           JOIN Teams AS T ON T.TEAM_CODE = M.TEAM_CODE
-             WHERE TEAM_NAME = Ravens;
+             WHERE TEAM_NAME = 'Ravens';
 
 /* Counting the number of players in the CowBoys Team*/
-2. SELECT COUNT(PLAYER_NUM )
+ SELECT COUNT(PLAYER_NUM )
        FROM Players AS P JOIN Members AS M ON P.MEMBER_NUM = M.MEMBER_NUM 
          JOIN Teams AS T ON T.TEAMCODE = M.TEAM_CODE
-            WHERE TEAM_NAME = COWBOYS;
+            WHERE TEAM_NAME = 'CowBoys';
 
 /* Displaying the players in the team that are in California*/
-3. SELECT PLAYER_NUM
+ SELECT PLAYER_NUM
       FROM   
  
