132775ad9f2584568cbf6a16cfe2bc9ea1b194ef
Matthew Olker <molker@highpoint.edu>
2019-04-28 21:09:20 -0400

    Fixed number 2

--- Queries.sql
+++ Queries.sql
@@ -8,10 +8,11 @@ WHERE TEAM_NAME = "Ravens";
 
 /* Counting the number of players in the CowBoys Team*/
 # 2. 
-SELECT COUNT(PLAYER_NUM )
-       FROM Players AS P JOIN Members AS M ON P.MEMBER_NUM = M.MEMBER_NUM 
-         JOIN Teams AS T ON T.TEAMCODE = M.TEAM_CODE
-            WHERE TEAM_NAME = 'Cowboys';
+SELECT COUNT(PLAYER_NUM) AS "Number of Players in Cowboys"
+FROM players AS P 
+JOIN members AS M ON P.MEMBER_NUM = M.MEMBER_NUM 
+JOIN teams AS T ON T.TEAMCODE = M.TEAM_CODE
+WHERE TEAM_NAME = "Cowboys";
 
 
  
