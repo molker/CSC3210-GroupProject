afc2b0fa3390c222a874a1f63b574cf8b118f027
Matthew Olker <molker@highpoint.edu>
2019-04-28 21:07:47 -0400

    Auto stash before merge of "master" and "origin/master"

--- Queries.sql
+++ Queries.sql
@@ -1,16 +1,17 @@
 /* Finding the cost of the Ravens Team*/
- SELECT OWNER_BUYPRICE
-       FROM Owners AS O JOIN Members AS M ON O.MEMBER_NUM = M.MEMBER_NUM 
-          JOIN Teams AS T ON T.TEAM_CODE = M.TEAM_CODE
-             WHERE TEAM_NAME = 'Ravens';
+# 1. 
+SELECT CONCAT("$", FORMAT(O.OWNER_BUYPRICE,2)) AS "Ravens Buy Price"
+FROM owners AS O 
+JOIN members AS M ON O.MEMBER_NUM = M.MEMBER_NUM 
+JOIN teams AS T ON T.TEAM_CODE = M.TEAM_CODE
+WHERE TEAM_NAME = "Ravens";
 
 /* Counting the number of players in the CowBoys Team*/
- SELECT COUNT(PLAYER_NUM )
+# 2. 
+SELECT COUNT(PLAYER_NUM )
        FROM Players AS P JOIN Members AS M ON P.MEMBER_NUM = M.MEMBER_NUM 
          JOIN Teams AS T ON T.TEAMCODE = M.TEAM_CODE
             WHERE TEAM_NAME = 'Cowboys';
 
-/* Displaying the players in the team that are in California*/
- SELECT PLAYER_NUM
-      FROM   
+
  
